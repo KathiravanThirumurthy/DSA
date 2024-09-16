20c9ff070f56d93cc5463bc41614121f66626017
Kathiravan Thirumurthy <tkathiravan1976@gmail.com>
2024-09-17 00:47:04 +0530

    Update Binary.js

--- DSA_Javascript/Searching/Binary.js
+++ DSA_Javascript/Searching/Binary.js
@@ -1,3 +1,4 @@
+// function declaration
 function binarySearch(arr, no) {
     let low = 0;
     let high = arr.length - 1;
@@ -6,7 +7,8 @@ function binarySearch(arr, no) {
         let middle = Math.floor((low + high) / 2);
 
         if (arr[middle] === no) {
-            return `Element found at index ${middle}`;
+          //  return `Element found at index ${middle}`;
+          return middle;
         }
         else if (arr[middle] < no) {
             low = middle + 1;
@@ -16,10 +18,19 @@ function binarySearch(arr, no) {
         }
     }
 
-    return "Element not found";
+   // return "Element not found";
+   return -1;
 }
 
 const arr = [5, 9, 17, 23, 25, 45, 59, 63, 71, 89];  // Sorted array
 const no = 59;  // Target value
 
-console.log(binarySearch(arr, no));  // Output: Element found at index 6
+//console.log(binarySearch(arr, no));  // Output: Element found at index 6
+let value=binarySearch(arr,no);
+console.log(value);
+if(value == -1)
+{
+    console.log("The Element not found");
+}else{
+    console.log(`The element is found at the index ${value} and the element is ${arr[value]}`)
+}
